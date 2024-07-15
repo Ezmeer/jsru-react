46286092f278f62ef56a4bc8f547ababd5b515d4
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-07-15 19:06:20 -0400

    add layout

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,15 @@
 import {Restaurant} from "./Restaurant/restaurant.jsx";
 import {restaurants} from "../mock.js";
+import {Layout} from "./layout.jsx";
 
 export const App = () => {
   return (
-    <>
-      {restaurants.map(restaurant => (
-        <Restaurant key={restaurant.id} restaurant={restaurant} />
-      ))}
-    </>
+    <div>
+        <Layout>
+            {restaurants.map(restaurant => (
+            <Restaurant key={restaurant.id} restaurant={restaurant} />
+             ))}
+        </Layout>
+    </div>
   );
 };
